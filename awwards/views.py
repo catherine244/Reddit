c7a5662a7d14ac9244fbcb0bd3f3c23b0608db7f
catherine244 <ccayreen24@gmail.com>
2020-02-16 12:32:48 +0300

    added request def in views.py

--- awwards/views.py
+++ awwards/views.py
@@ -4,3 +4,9 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    date = dt.date.today()
+    projects = Projects.get_projects()
+    
+
+    return render(request, 'index.html', {"date": date, "projects":projects})
