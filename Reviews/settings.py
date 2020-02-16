72479e2514d0eb70d4fc7349defc3823ac55a4d5
catherine244 <ccayreen24@gmail.com>
2020-02-16 15:48:57 +0300

    added database  in settings.py

--- Reviews/settings.py
+++ Reviews/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'Reviews.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'awa',
+        'USER': 'cate',
+        'PASSWORD':'cate4321',
     }
 }
 
